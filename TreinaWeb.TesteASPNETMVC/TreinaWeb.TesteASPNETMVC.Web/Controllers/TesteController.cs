160ed97ddf1922dec5008c6eb8ddcbe28b86cd7d
rodriguesdev <leandro.lacr@yahoo.com.br>
2016-03-12 16:19:14 -0300

    Estudos

---
+++ TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Controllers/TesteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TreinaWeb.TesteASPNETMVC.Web.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            string nome = form["nome"];
+            ViewBag.Mensagem = nome;
+            return View("Saudacao");
+        }
+    }
+}
